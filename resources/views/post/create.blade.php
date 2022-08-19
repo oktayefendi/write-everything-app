ee249485fd86f8dafff90d3e051db3f4de17b482
oktayefendi <efendicod@gmail.com>
2022-08-19 20:22:07 +0400

    Migrations Created

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -11,7 +11,6 @@
               <h2 for="message">Your Message</h2>
               <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="What do you think?"></textarea>
             </div>
-
             <button type="submit" class="btn btn-primary m-2">Shout!</button>
           </form>
     </div>
